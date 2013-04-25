eaac0fd37b025078935e4ed290fe3bea6dee8b8b
Anders Asplund <aasplund@gmail.com>
2013-04-25 11:28:11 +0200

    working manual transfer

--- src/main/java/com/mk/camel/processor/ManualTransferProcessor.java
+++ src/main/java/com/mk/camel/processor/ManualTransferProcessor.java
@@ -1,6 +1,7 @@
 package com.mk.camel.processor;
 
 import com.mk.services.model.TransferJob;
+import com.mk.services.model.TransferParams;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 
@@ -10,23 +11,20 @@ import java.util.List;
 public class ManualTransferProcessor implements Processor {
     @Override
     public void process(Exchange exchange) throws Exception {
-        final Object start = exchange.getIn().getHeader("start");
-        final Object end = exchange.getIn().getHeader("end");
+        final TransferParams params = (TransferParams) exchange.getIn().getBody();
 
-        final String transfertypes = (String) exchange.getIn().getHeader("transfertypes");
-        final String[] transfers = transfertypes.split(",");
 
-        final List<TransferJob> params = new ArrayList<TransferJob>();
-        for (final String tt : transfers) {
+        final List<TransferJob> jobs = new ArrayList<TransferJob>();
+        for (final String tt : params.getTransferTypes()) {
             final TransferJob job = new TransferJob();
-            job.setStart(start.toString());
-            job.setEnd(end.toString());
+            job.setStart(params.getStart());
+            job.setEnd(params.getEnd());
             job.setTransferType(tt);
             job.setCrn("191212121212");
 
-            params.add(job);
+            jobs.add(job);
         }
 
-        exchange.getOut().setBody(params);
+        exchange.getOut().setBody(jobs);
     }
 }
